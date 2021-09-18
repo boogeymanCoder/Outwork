c36c09d591134d83688eb4607c3c75125155da68
boogeymanCoder <72642782+boogeymanCoder@users.noreply.github.com>
2021-09-18 16:19:19 +0800

    temporery disable prompt

--- public/sw_setup.js
+++ public/sw_setup.js
@@ -6,16 +6,16 @@ if('serviceWorker' in navigator) {
     });
 }
 
-window.addEventListener('beforeinstallprompt', (deferredPrompt) => {
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-        } else {
-            console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-    });
-});
+// window.addEventListener('beforeinstallprompt', (deferredPrompt) => {
+//     // Show the prompt
+//     deferredPrompt.prompt();
+//     // Wait for the user to respond to the prompt
+//     deferredPrompt.userChoice.then((choiceResult) => {
+//         if (choiceResult.outcome === 'accepted') {
+//             console.log('User accepted the A2HS prompt');
+//         } else {
+//             console.log('User dismissed the A2HS prompt');
+//         }
+//         deferredPrompt = null;
+//     });
+// });
