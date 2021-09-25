a76ed79cb10205b49281d75f896b4e41462c04aa
boogeymanCoder <72642782+boogeymanCoder@users.noreply.github.com>
2021-09-25 18:44:46 +0800

    added update modal

--- routes/application.js
+++ routes/application.js
@@ -25,4 +25,25 @@ router.delete('/cancel/:application_id', async (req, res) => {
     res.redirect(`/job/view/${jobId}`);
 });
 
+router.patch('/update/:application_id', async (req, res) => {
+    const application = await Application.findById(req.params.application_id);
+
+    if (application === null) {
+        req.flash('error', 'Invalid URL');
+        return res.redirect('/');
+    }
+
+    if(application.employee !== req.user.username) {
+        req.flash('error', 'Update denied, application not owned');
+        return res.redirect(`/job/view/${application.jobId}`);
+    }
+
+    application.message = req.body.message;
+    application.time = new Date();
+    await application.save();
+
+    req.flash('info', 'Updated successfuly');
+    res.redirect(`/job/view/${application.jobId}`);
+});
+
 module.exports = router;
