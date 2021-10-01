dcb9bf5f89d75bc3c5b42dd8251a6356b5828541
boogeymanCoder <johnbryanpitcasoy@gmail.com>
2021-10-01 21:28:50 +0800

    better insight parallax content

--- routes/public.js
+++ routes/public.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/insights', (req, res) => {
-    res.render('public/insights.ejs', { auth: req.isAuthenticated() });
+    res.render('public/insights.ejs', { auth: req.isAuthenticated(), hideFooter: true });
 });
 
 module.exports = router;
